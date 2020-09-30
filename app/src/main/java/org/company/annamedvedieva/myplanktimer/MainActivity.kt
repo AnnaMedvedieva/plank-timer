f961da7250bd8a1995a228659d412b1565138084
Anna Medvedieva <annamedvedieva.dev@gmail.com>
2020-09-30 11:14:25 +0100

    Set up SmoothBottomBar library

--- app/src/main/java/org/company/annamedvedieva/myplanktimer/MainActivity.kt
+++ app/src/main/java/org/company/annamedvedieva/myplanktimer/MainActivity.kt
@@ -2,10 +2,26 @@ package org.company.annamedvedieva.myplanktimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        bottomBar.setupWithNavController(menu!!, navController)
+        return true
     }
 }
